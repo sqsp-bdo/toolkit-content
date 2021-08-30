271439ae34644d510884857f9ddb476c9a44d20e
sqsp-bdo <59670488+sqsp-bdo@users.noreply.github.com>
2021-08-30 13:17:12 -0400

    Update js-10kb.js

--- livechat-simulation/js-10kb.js
+++ livechat-simulation/js-10kb.js
@@ -1,1 +1,8 @@
-console.log("This is a test!");
+function reqListener () {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "/api/3/commerce/products/?productTypes=PHYSICAL");
+oReq.send();
